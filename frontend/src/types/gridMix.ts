c1f390a933a20a0196254f7c23f31198ae9f5360
Austen Sorochak <austensorochak@gmail.com>
2025-04-19 12:30:49 -0700

    add GridMixEntry type

---
+++ frontend/src/types/gridMix.ts
@@ -0,0 +1,12 @@
+export type GridMixEntry = {
+  timestamp: string;
+  period: string;
+  respondent: string;
+  "respondent-name": string;
+  fueltype: string;
+  "type-name": string;
+  timezone: string;
+  "timezone-description": string;
+  "Generation (MWh)": number;
+  "value-units": string;
+};
