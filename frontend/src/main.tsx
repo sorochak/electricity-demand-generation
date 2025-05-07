e45700a38127182c9fa1167c7cd2c7d8b4372023
Austen Sorochak <austensorochak@gmail.com>
2025-05-07 15:12:17 -0700

    enable dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,24 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "leaflet/dist/leaflet.css";
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </StrictMode>
   );
 }
